b8ec4d06bbb65452cba426f6087c243eaefdd519
Kyle Copenhaver <kncopen@gmail.com>
2023-08-12 20:20:25 -0400

    Add project files.

---
+++ Kards/Kard.cs
@@ -0,0 +1,126 @@
+﻿
+
+namespace Kards
+{
+    public enum KardRank
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+
+    public enum KardSuit
+    {
+        Club = 1,
+        Diamond = 2,
+        Heart = 3,
+        Spade = 4
+    }
+
+    public enum KardColor
+    { 
+        Black,
+        Red
+    }
+    public class Kard
+    {
+        public KardSuit Suit { get; set; }
+        public KardRank Rank { get; set; }
+
+        public int KardID { get; }
+       
+        public Kard(int suit, int rank)
+        {
+            this.Suit = (KardSuit)suit;
+            this.Rank = (KardRank)suit;
+            this.KardID = ((int)suit - 1) * 13 + (int)rank;
+        }
+    
+
+        public KardColor Color() 
+        {
+            if (Suit == KardSuit.Club || Suit == KardSuit.Spade) return KardColor.Black;
+            else return KardColor.Red; 
+        }
+
+        override
+        public String ToString() 
+        {
+            String suit, rank;
+            switch (this.Suit)
+            {
+                case KardSuit.Club:
+                    suit = "Club";
+                    break;
+                case KardSuit.Spade:
+                    suit = "Spade";
+                    break;
+                case KardSuit.Heart:
+                    suit = "Heart";
+                    break;
+                case KardSuit.Diamond:
+                    suit = "Diamond";
+                    break;
+                default:
+                    return "Invalid Card";
+            }
+            switch (this.Rank)
+            {
+                case KardRank.Ace:
+                    rank = "Ace";
+                    break;
+                case KardRank.Two:
+                    rank = "Two";
+                    break;
+                case KardRank.Three:
+                    rank = "Three";
+                    break;
+                case KardRank.Four:
+                    rank = "Four";
+                    break;
+                case KardRank.Five:
+                    rank = "Five";
+                    break;
+                case KardRank.Six:
+                    rank = "Six";
+                    break;
+                case KardRank.Seven:
+                    rank = "Seven";
+                    break;
+                case KardRank.Eight:
+                    rank = "Eight";
+                    break;
+                case KardRank.Nine:
+                    rank = "Nine";
+                    break;
+                case KardRank.Ten:
+                    rank = "Ten";
+                    break;
+                case KardRank.Jack:
+                    rank = "Jack";
+                    break;
+                case KardRank.Queen:
+                    rank = "Queen";
+                    break;
+                case KardRank.King:
+                    rank = "King";
+                    break;
+                default:
+                    return "Invalid Card";
+            }
+
+            return ("[" + this.KardID + "] " + rank + " of " + suit + "s."); 
+        }
+
+    }
+}
---
+++ Kards/KardDeck.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Authentication.OAuth.Claims;
+
+namespace Kards
+{
+    public class KardDeck
+    {
+        public Kard[] Deck = new Kard[52];
+
+        private int CurrentDeckSize { get; set; }
+        public KardDeck()
+        {
+            this.CurrentDeckSize = 52;
+            //init cards in deck
+            for(int i = 1; i <= 4; i++)
+                for (int j = 1; j <= 13; j++)
+                {
+                    this.Deck[(i-1)*13 + (j - 1)] = new Kard(i, j);
+                }
+            
+        }
+
+        //returns a card from the deck but does not remove it
+        //TIME:O(1)
+        public Kard PeakKard(int index)
+        {
+            Kard kard;
+            if (index >= this.CurrentDeckSize) kard = this.Deck[this.CurrentDeckSize - 1];
+            else if (index < 0) kard = this.Deck[0];
+            else kard = this.Deck[index];
+            return kard;
+        }
+
+        //TIME:O(1)
+        public Kard PeakTopKard() 
+        {
+            return this.Deck[this.CurrentDeckSize - 1];
+        }
+
+        //TIME:O(1)
+        public Kard PeakBottomKard()
+        {
+            return this.Deck[0];
+        }
+
+        //remove and return a card from the deck 
+        //TIME:O(n)
+        public Kard DrawKard(int index) 
+        {
+            Kard kard = PeakKard(index);
+            //make sure the stack of cards currently in the deck is at the start of the array
+            for (int i = index + 1; i < this.CurrentDeckSize; i++)
+            {
+                SwapKardPositions(i, i - 1);
+            }
+
+            this.CurrentDeckSize -= 1;
+            return kard;
+        }
+
+        //TIME:O(1)
+        public Kard DrawTopKard() 
+        {
+            return DrawKard(this.CurrentDeckSize);
+        }
+
+        //TIME:O(n)
+        public Kard DrawBottomKard()
+        {
+            return DrawKard(0);
+        }
+
+        //TIME:O(1)
+        public void SwapKardPositions(int index1, int index2)
+        {
+            (this.Deck[index2], this.Deck[index1]) = (this.Deck[index1], this.Deck[index2]);
+        }
+
+        //TIME: O(n) SPACE: O(n)
+        public void Shuffle()
+        {
+            Random rnd = new();
+            
+            int randIndex;
+            for ( int i = 0; i < this.CurrentDeckSize; i++)
+            {
+                randIndex = rnd.Next(0, this.CurrentDeckSize);
+                SwapKardPositions(i, randIndex);
+            }
+            
+        }
+
+        override
+        public String ToString()
+        {
+            String deckstr = "{\n\t";
+            for(int i = 0; i < this.CurrentDeckSize; i++)
+            {
+                deckstr += Deck[i].ToString();
+                if(i < this.CurrentDeckSize - 1) deckstr += ",\n\t";
+            }
+            deckstr += "\n}";
+            return deckstr;
+        }
+
+    }
+}
